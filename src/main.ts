43ed4385d2169103c53763b94d17b29700e17913
Iago Saraiva <iago.saraiva@pmovil.com.br>
2025-11-14 16:22:22 -0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.ALLOWED_ORIGINS?.split(',') || [
       'http://localhost:3000',
+      'https://opusatlas.com.br/',
     ],
     credentials: true,
   });
